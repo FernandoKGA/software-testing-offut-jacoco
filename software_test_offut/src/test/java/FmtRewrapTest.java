6d2bb856d2d06de5255423879fcadca08522ed10
Fernando Karchiloff <fernando.kga@gmail.com>
2025-05-11 20:06:38 +0000

    Adding some node coverage tests (maybe wrong)

--- software_test_offut/src/test/java/FmtRewrapTest.java
+++ software_test_offut/src/test/java/FmtRewrapTest.java
@@ -5,11 +5,48 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class FmtRewrapTest {
 
-    //ADD AQUI OS TESTES DE VERDADE
+    //#region Node coverage
+    
+    
+    // [1,2,3,5,7,8,10,17,18,2,19]
+    // [1,2,3,5,7,9,10,17,18,2,19]
+    
+    //[1,2,3,4,10,11,18,2,19]
     @Test
-    public void dummyCoverage() {
-        FmtRewrap.fmtRewrap("TESTE DUMMY", 10);
-        assertTrue(true);
+    public void firstNodeCoverage() {
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> FmtRewrap.fmtRewrap("  ", 2));
     }
 
+    //[1,2,3,4,10,12,18,2,19]
+    @Test
+    public void secondNodeCoverage() {
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> FmtRewrap.fmtRewrap("  ", 1));
+    }
+
+    // [1,2,3,4,10,13,14,18,2,19]
+    @Test
+    public void thirdNodeCoverage() {
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> FmtRewrap.fmtRewrap(" \n", 1));
+    }
+
+    // [1,2,3,4,10,13,15,18,2,19]
+    @Test
+    public void fourthNodeCoverage() {
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> FmtRewrap.fmtRewrap("A", 1));
+    }
+
+    // [1,2,3,4,10,16,17,18,2,19]
+    @Test
+    public void fifthNodeCoverage() {
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> FmtRewrap.fmtRewrap("AA", 1));
+    }
+
+    // [1,2,3,5,6,10,17,18,2,19]
+    @Test
+    public void sixNodeCoverage() {
+        assertEquals(" \n", FmtRewrap.fmtRewrap("\n", 4));
+    }
+
+    
+    //#endregion
 }
